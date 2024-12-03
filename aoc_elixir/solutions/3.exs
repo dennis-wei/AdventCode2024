a73ef9dbc95c16078f71087f9ec7499dccb38fb0
Dennis Wei <dw2654@columbia.edu>
2024-12-03 00:47:44 -0500

    cleaner day 3

--- aoc_elixir/solutions/3.exs
+++ aoc_elixir/solutions/3.exs
@@ -13,30 +13,22 @@ defmodule Day3 do
       # .ints_in_lines(filename)
   end
 
+  @mul_pattern ~r/mul\(([0-9]{1,3}),([0-9]{1,3})\)/
+  @dos_pattern ~r/don't\(\).*?(do\(\)|$)/
+
+  def run(str) do
+    Regex.scan(@mul_pattern, str)
+    |> Enum.map(fn [_, l, r] -> String.to_integer(l) * String.to_integer(r) end)
+    |> Enum.sum()
+  end
+
   def solve(test \\ false) do
     input = get_input(test)
-    pattern = ~r/mul\(([0-9]{1,3}),([0-9]{1,3})\)|do\(\)|don't\(\)/
-    ops = Regex.scan(pattern, input)
-    |> Enum.map(fn args ->
-      case args do
-        ["do()"] -> :do
-        ["don't()"] -> :dont
-        [_, l, r] -> String.to_integer(l) * String.to_integer(r)
-      end
-    end)
-    part1 = ops
-    |> Enum.filter(fn op -> !is_atom(op) end)
-    |> Enum.sum
-    part2 = ops
-    |> Enum.reduce({true, 0}, fn op, {is_on, acc} ->
-      case op do
-        :do -> {true, acc}
-        :dont -> {false, acc}
-        n when is_integer(n) and is_on -> {true, acc + n}
-        n when is_integer(n) and not(is_on) -> {false, acc}
-      end
-    end)
-    |> elem(1)
+
+    part1 = run(input)
+
+    cleaned = Regex.replace(@dos_pattern, input, "")
+    part2 = run(cleaned)
     IO.puts("Part 1: #{part1}")
     IO.puts("Part 2: #{part2}")
   end
