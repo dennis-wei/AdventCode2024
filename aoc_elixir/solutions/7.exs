704dc7f7a59c8b9bf1f9dc3beeaa7e5d2673732a
Dennis Wei <dw2654@columbia.edu>
2024-12-07 14:13:33 -0500

    day 7 recurse

--- aoc_elixir/solutions/7.exs
+++ aoc_elixir/solutions/7.exs
@@ -16,60 +16,38 @@ defmodule Day7 do
     String.to_integer(Integer.to_string(n1) <> Integer.to_string(n2))
   end
 
-  def get_possibilities(n_ops, part2 \\ false) do
-    ops = case part2 do
-      false -> [&+/2, &*/2]
-      true -> [&+/2, &*/2, &concat/2]
+  def recurse(target, ops, acc, op, remaining) do
+    [n | r] = remaining
+    nacc = apply(op, [acc, n])
+    cond do
+      length(r) == 0 and nacc == target -> true
+      length(r) == 0 -> false
+      nacc > target -> false
+      true -> Enum.any?(ops, fn next_op -> recurse(target, ops, nacc, next_op, r) end)
     end
-    Enum.reduce(2..n_ops, Enum.map(ops, fn op -> [op] end), fn _, acc ->
-      Comb.cartesian_product(acc, ops)
-      |> Enum.map(&List.flatten/1)
-    end)
   end
 
-  def is_valid(target, nums, ops) do
-    [initial | remaining] = nums
-    res = Enum.zip(remaining, ops)
-    |> Enum.reduce_while(initial, fn {new_n, op}, acc ->
-      res = apply(op, [acc, new_n])
-      cond do
-        res > target -> {:halt, false}
-        true -> {:cont, res}
-      end
-    end)
-    target == res
-  end
-
-  def is_possible(line, precom_possibilities) do
-    [target | nums] = line
-    num_ops = length(nums)
-    possibilities = Map.get(precom_possibilities, num_ops)
-    Enum.any?(possibilities, fn possibility -> is_valid(target, nums, possibility) end)
+  def driver(target, ops, nums) do
+    [first | rest] = nums
+    Enum.any?(ops, fn op -> recurse(target, ops, first, op, rest) end)
   end
 
   def solve(test \\ false) do
     input = get_input(test)
-    max_ops = input
-    |> Enum.map(&length/1)
-    |> Enum.max()
-    |> then(fn n -> n - 1 end)
 
-    precomputed_combos1 = Enum.reduce(2..max_ops, %{}, fn n, acc -> Map.put(acc, n, get_possibilities(n)) end)
-    part1 = Enum.map(input, fn i -> Task.async(fn -> {hd(i), is_possible(i, precomputed_combos1)} end) end)
-    |> Task.await_many(30000)
-    |> Enum.filter(&elem(&1, 1))
-    |> Enum.map(&elem(&1, 0))
+    part1 = input
+    |> Enum.filter(fn [target | nums] -> driver(target, [&+/2, &*/2], nums) end)
+    |> Enum.map(fn [target | _] -> target end)
     |> Enum.sum()
 
-    precomputed_combos2 = Enum.reduce(2..max_ops, %{}, fn n, acc -> Map.put(acc, n, get_possibilities(n, true)) end)
-    part2 = Enum.map(input, fn i -> Task.async(fn -> {hd(i), is_possible(i, precomputed_combos2)} end) end)
-    |> Task.await_many(30000)
-    |> Enum.filter(&elem(&1, 1))
-    |> Enum.map(&elem(&1, 0))
+    part2 = input
+    |> Enum.filter(fn [target | nums] -> driver(target, [&+/2, &*/2, &concat/2], nums) end)
+    |> Enum.map(fn [target | _] -> target end)
     |> Enum.sum()
+
     IO.puts("Part 1: #{part1}")
     IO.puts("Part 2: #{part2}")
   end
 end
 
-Day7.solve(false)
+Day7.solve()
